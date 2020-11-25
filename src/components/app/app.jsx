48e7fdc9e7f52f50d150543e24775fdd67278a08
julia-marta <marta237@yandex.ru>
2020-11-25 05:16:12 +0300

    Устанавливает gh pages, прописывает скрипты, меняет history на hash history

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -15,7 +15,7 @@ const {ROOT, LOGIN, MY_LIST, FILMS, REVIEW, PLAYER} = AppRoute;
 const App = () => {
 
   return (
-    <BrowserRouter history={browserHistory}>
+    <BrowserRouter basename={ROOT} history={browserHistory}>
       <Switch>
         <Route exact path={ROOT} render={({history}) => (
           <Main onPlayClick={(id) => history.push(`${PLAYER}/${id}`)}/>
